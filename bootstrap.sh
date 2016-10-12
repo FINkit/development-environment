53f597af89b6f30657b7191e3ed0fd8c7afed567
cagiti <caicooper82@gmail.com>
2016-10-12 16:04:12 +0100

    Removed VirtualBox Guest Additions - this is preinstalled in packer base box

--- bootstrap.sh
+++ bootstrap.sh
@@ -15,21 +15,6 @@ then
   exit 0
 fi
 
-touch /var/vagrant_provision
-
-###############################################################################################
-## Install VirtualBox Guest Additions                                                        ##
-###############################################################################################
-sudo apt-get -y --force-yes -qq install build-essential linux-headers-`uname -r`
-sudo apt-get -y --force-yes -qq install xserver-xorg xserver-xorg-core
-wget http://download.virtualbox.org/virtualbox/4.3.30/VBoxGuestAdditions_4.3.30.iso
-sudo mkdir /media/VBoxGuestAdditions
-sudo mount -o loop,ro VBoxGuestAdditions_4.3.30.iso /media/VBoxGuestAdditions
-sudo sh /media/VBoxGuestAdditions/VBoxLinuxAdditions.run
-rm VBoxGuestAdditions_4.3.30.iso
-sudo umount /media/VBoxGuestAdditions
-sudo rmdir /media/VBoxGuestAdditions
-
 ###############################################################################################
 ## Install upstart for use with ubuntu vivid OS                                              ##
 ###############################################################################################
